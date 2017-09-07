eea9c742479a1a737ac6c0cae97ce58da3a3bff5
Fabio <fcor14@hotmail.com>
2017-09-07 14:11:23 -0500

    BLE support added

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
+"use strict";
+
 const app = require('express')()
 const http = require('http').Server(app)
-var io = require('socket.io')(http)
+const io = require('socket.io')(http)
+const Bean = require('ble-bean')
+let intervalId, connectedBean
 
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', function(socket){
-  socket.on('numClick', function(msg){
-    console.log('message: ' + msg)
-  })
-  setTimeout(function() {
-    io.emit('getLux', 'Works! 3 secs later')
-  } , 3000);
+  console.log('User connected');
+  // setTimeout(function() {
+  //   io.emit('getLux', 'Works! 3 secs later')
+  // } , 3000);
+
+  Bean.discover(function(bean){
+    connectedBean = bean;
+    process.on('SIGINT', exitHandler.bind(this));
+
+    bean.on("serial", function(data, valid){
+      const lux = parseInt(data.toString());
+      io.emit('getLux', lux)
+    });
+
+
+    bean.on("disconnect", function(){
+      process.exit();
+    });
+
+    bean.connectAndSetup(function(){
+      //Here goes data send over scratch caracteristic
+    });
+
+  });
+
+  process.stdin.resume();//so the program will not close instantly
+  var triedToExit = false;
+
+  //turns off led before disconnecting
+  var exitHandler = function exitHandler() {
+
+    var self = this;
+    if (connectedBean && !triedToExit) {
+      triedToExit = true;
+      console.log('Turning off led...');
+      clearInterval(intervalId);
+      connectedBean.setColor(new Buffer([0x0,0x0,0x0]), function(){});
+      //no way to know if succesful but often behind other commands going out, so just wait 2 seconds
+      console.log('Disconnecting from Device...');
+      setTimeout(connectedBean.disconnect.bind(connectedBean, function(){}), 2000);
+    } else {
+      process.exit();
+    }
+  };
+
 })
 
 http.listen(3000, function(){
